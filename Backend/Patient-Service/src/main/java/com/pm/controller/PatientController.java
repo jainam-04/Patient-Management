99015c2c9de54e5133ddedc31e29b75a43089922
Jainam Rupani <jainamrupani04@gmail.com>
2025-12-13 23:22:12 +0530

    Created Patient Controller

---
+++ Backend/Patient-Service/src/main/java/com/pm/controller/PatientController.java
@@ -0,0 +1,13 @@
+package com.pm.controller;
+
+import com.pm.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    @Autowired
+    private PatientService patientService;
+}
