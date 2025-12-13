15467f148447e900c80267f9cd367b5fe00bd48d
Jainam Rupani <jainamrupani04@gmail.com>
2025-12-13 23:12:54 +0530

    Created Patient Mapper

---
+++ Backend/Patient-Service/src/main/java/com/pm/mapper/PatientMapper.java
@@ -0,0 +1,16 @@
+package com.pm.mapper;
+
+import com.pm.dto.PatientResponseDTO;
+import com.pm.model.Patient;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient){
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
+        patientResponseDTO.setId(patient.getId().toString());
+        patientResponseDTO.setName(patient.getName());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientResponseDTO;
+    }
+}
