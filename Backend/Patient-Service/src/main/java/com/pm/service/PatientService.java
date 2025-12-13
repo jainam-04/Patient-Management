730c75dc660a9ab69f1b2beaa4f48dbcfb141ba2
Jainam Rupani <jainamrupani04@gmail.com>
2025-12-13 22:28:49 +0530

    Created Patient Service

---
+++ Backend/Patient-Service/src/main/java/com/pm/service/PatientService.java
@@ -0,0 +1,11 @@
+package com.pm.service;
+
+import com.pm.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
+}
