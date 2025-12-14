88db0236b6ccd3069cbed382afe9b3809777a46f
Jainam Rupani <jainamrupani04@gmail.com>
2025-12-14 12:43:12 +0530

    Created Get Mapping to Get All Patients

--- Backend/Patient-Service/src/main/java/com/pm/controller/PatientController.java
+++ Backend/Patient-Service/src/main/java/com/pm/controller/PatientController.java
@@ -1,13 +1,24 @@
 package com.pm.controller;
 
+import com.pm.dto.PatientResponseDTO;
 import com.pm.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
     @Autowired
     private PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients(){
+        List<PatientResponseDTO> patientResponseDTOS = patientService.getAllPatients();
+        return ResponseEntity.ok().body(patientResponseDTOS);
+    }
 }
--- Backend/Patient-Service/src/main/java/com/pm/service/PatientService.java
+++ Backend/Patient-Service/src/main/java/com/pm/service/PatientService.java
@@ -1,11 +1,21 @@
 package com.pm.service;
 
+import com.pm.dto.PatientResponseDTO;
+import com.pm.mapper.PatientMapper;
+import com.pm.model.Patient;
 import com.pm.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
+
+    public List<PatientResponseDTO> getAllPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
 }
