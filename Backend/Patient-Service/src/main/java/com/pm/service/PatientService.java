0aeaf3e83e1c97eca6a4beb73868bf55a3f22626
Jainam Rupani <jainamrupani04@gmail.com>
2025-12-14 23:01:54 +0530

    Created Put Mapping to Update Patient and Created Custom Exception Patient Not Found Exception

--- Backend/Patient-Service/src/main/java/com/pm/service/PatientService.java
+++ Backend/Patient-Service/src/main/java/com/pm/service/PatientService.java
@@ -3,13 +3,16 @@ package com.pm.service;
 import com.pm.dto.PatientRequestDTO;
 import com.pm.dto.PatientResponseDTO;
 import com.pm.exception.EmailAlreadyExistsException;
+import com.pm.exception.PatientNotFoundException;
 import com.pm.mapper.PatientMapper;
 import com.pm.model.Patient;
 import com.pm.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -27,5 +30,18 @@ public class PatientService {
         }
         Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(patient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient not found with ID: " + id));
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with this email already exists " + patientRequestDTO.getEmail());
+        }
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        Patient updatedPatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(updatedPatient);
     }
 }
